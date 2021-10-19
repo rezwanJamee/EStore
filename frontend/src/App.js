1f5b497e6bdbed1304cfc051e884ccfb39d68b9f
rezwanjamee <rezwan.jamee@gmail.com>
2021-10-19 14:02:31 +1100

    Minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,51 +26,51 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">eCommerce Store</Link>
-        </div>
-        <div>
-          <Link to="/cart">
-            Cart
-            {cartItems.length > 0 && (
-              <span className="badge">{cartItems.length}</span>
-            )}
-          </Link>
-          {
-            userInfo ? (
-              <div className="dropdown">
-                <Link to="#">
-                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="#signout" onClick={signoutHandler}>
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to="/signin">Sign In</Link>
-            )
-          }         
-        </div>
-      </header>
-      <main>
-        <Route path="/cart/:id?" component={CartScreen}></Route>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path="/signin" component={SigninScreen}></Route>
-        <Route path="/register" component={RegisterScreen}></Route>
-        <Route path="/shipping" component={ShippingAddressScreen}></Route>
-        <Route path="/payment" component={PaymentMethodScreen}></Route>
-        <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">eCommerce Store</Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">
+                    {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                  </Link>
+                  <ul className="dropdown-content">
+                    <li>
+                      <Link to="#signout" onClick={signoutHandler}>
+                        Sign Out
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <Link to="/signin">Sign In</Link>
+              )
+            }         
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
 
-        <Route path="/" component={HomeScreen} exact></Route> 
-        
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+          <Route path="/" component={HomeScreen} exact></Route> 
+          
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </div>
     </BrowserRouter>
   );
